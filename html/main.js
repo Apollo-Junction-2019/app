be8f46127aa27338062b38c7038a8afc647e76e9
ahnl <you@example.com>
2019-11-16 15:50:08 +0200

    Added a function to interact with the API

--- html/main.js
+++ html/main.js
@@ -5,6 +5,21 @@ function addMessage(isRecipient, message) {
         scrollTop: $('#chatArea').get(0).scrollHeight
     }, 500);
 }
+//ask the ai
+function sendRequest(msg, cb) {
+
+    // MAKE SURE TO ENABLE CORS policy 'Access-Control-Allow-Origin'
+
+      $.ajax({
+        url: 'https://some.address/pong.php?q=' + msg
+      })
+        .done(function( data ) {
+          if ( console && console.log ) {
+            var json = JSON.parse(data);
+            addMessage(true, json.msg);
+          }
+        });
+}
 $("#messageBox").keyup(function(event) {
     if (event.keyCode === 13) {
         if ($('#messageBox').val().trim() != '') {
@@ -12,7 +27,12 @@ $("#messageBox").keyup(function(event) {
             if (event.shiftKey) {
                  isRecipient = true;
             } 
-            addMessage(isRecipient, $('#messageBox').val());
+            addMessage(false, $('#messageBox').val());
+
+            sendRequest( $('#messageBox').val(), function(reply) {
+                addMessage(isRecipient,reply);
+
+            });
             $('#messageBox').val('');
         }
     }
